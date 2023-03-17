2ff2e0c110835395a45ad8da369add839612edfc
Michael Feldstein <mfeldstein@figma.com>
2023-03-16 22:55:54 -0700

    Add missing file

---
+++ pages/api/_database.ts
@@ -0,0 +1,21 @@
+import { MongoClient, Db, Collection } from 'mongodb';
+
+let client : MongoClient | null = null;
+
+const getDatabase = async () => {
+  if (!client) {
+    console.log('Connecting to database', process.env.DATABASE_URL);
+    client = await MongoClient.connect(process.env.DATABASE_URL!, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  }
+  return client!.db();
+};
+
+const getCollection = async (name: string) => {
+  const database = await getDatabase();
+  return database.collection(name);
+};
+
+export { getCollection };
