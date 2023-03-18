0216a60378ec6dafe3b86007caf995bc43b31897
Michael Feldstein <mfeldstein@figma.com>
2023-03-17 22:00:22 -0700

    Make command+enter run the script, add favicon and title

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { useRef, useState } from "react";
 import Editor, { Monaco } from "@monaco-editor/react";
 import monaco from "monaco-editor";
 import { types } from "../util/p5types";
+import Head from "next/head";
 
 const prelude = `You are a creative coding assistant who is going to help me write p5js sketches.  Please respond only with the code that should be run, no explanations.  I will put the current code between [BEGIN] and [END] tokens, with the query of how i'd like you to modify the sketch below.  Be sure to only respond with the full representation of the modified code and no editorial or explanations.`;
 const sketchBegin = "[BEGIN]";
@@ -18,11 +19,33 @@ function draw() {
 }
 `;
 
+const placeholder = `What would you like to do to the sketch?
+
+Examples:
+- Make the circle blue
+- Add a bunch of particles that orbit around the circle
+- Add comments to the code
+- Pull all the constant numbers out to named variables so I can tweak them`;
+
+const questionPlaceholder = `Ask a question about the sketch
+
+Examples:
+- Why isn't the ball following my cursor
+- What does line 32 mean?
+- Explain the attractParticle() function`;
+
 export default function Home() {
   const iframeRef = useRef<HTMLIFrameElement>(null);
   const editorRef = useRef<monaco.editor.IStandaloneCodeEditor | null>(null);
   const textareaRef = useRef<HTMLTextAreaElement | null>(null);
+  const [questionMode, setQuestionMode] = useState(false);
   const [loading, setLoading] = useState(false);
+
+  function setQuestionModeAndClear(newQuestionMode: boolean) {
+    if (newQuestionMode === questionMode) return;
+    setQuestionMode(newQuestionMode);
+    textareaRef.current!.value = "";
+  }
 
   function handleEditorDidMount(
     editor: monaco.editor.IStandaloneCodeEditor,
@@ -30,6 +53,12 @@ export default function Home() {
   ) {
     editorRef.current = editor;
     play();
+    editor.addAction({
+      id: "run-sketch",
+      label: "Run Sketch",
+      keybindings: [monaco.KeyMod.CtrlCmd | monaco.KeyCode.Enter],
+      run: play,
+    });
     monaco.languages.typescript.javascriptDefaults.addExtraLib(types);
   }
 
@@ -76,43 +105,67 @@ export default function Home() {
   }
 
   return (
-    <div className="App">
-      <div className="EditorContainer">
-        <Editor
-          theme="vs-dark"
-          onMount={handleEditorDidMount}
-          defaultLanguage="javascript"
-          defaultValue={defaultScript}
-          options={{
-            minimap: { enabled: false },
-            automaticLayout: true,
-            language: "javascript",
-          }}
-        />
-      </div>
-      <div className="PreviewContainer">
-        <iframe
-          title="preview"
-          ref={iframeRef}
-          width={500}
-          height={500}
-        ></iframe>
-        <div id="controls">
-          <div className="button" onClick={play}>
-            Play
+    <>
+      {/* <div id="Header">Header</div> */}
+      <div className="App">
+        <Head>
+          <title>P5ai</title>
+          <link rel="icon" type="image/png" href="/icon-no-bg.png" />
+        </Head>
+
+        <div className="EditorContainer">
+          <Editor
+            theme="vs-dark"
+            onMount={handleEditorDidMount}
+            defaultLanguage="javascript"
+            defaultValue={defaultScript}
+            options={{
+              minimap: { enabled: false },
+              automaticLayout: true,
+              language: "javascript",
+            }}
+          />
+        </div>
+        <div className="PreviewContainer">
+          <iframe
+            title="preview"
+            ref={iframeRef}
+            width={500}
+            height={500}
+          ></iframe>
+          <div id="controls">
+            <div id="button-row">
+              {/* <button
+                disabled={loading}
+                className="button"
+                onClick={() => setQuestionModeAndClear(false)}
+              >
+                Alter
+              </button>
+              <button
+                disabled={loading}
+                className="button"
+                onClick={() => setQuestionModeAndClear(true)}
+              >
+                Ask
+              </button> */}
+              <button disabled={loading} className="button" onClick={play}>
+                Play
+              </button>
+            </div>
+            <div className="PromptContainer">
+              <textarea
+                rows={9}
+                disabled={loading}
+                ref={textareaRef}
+                onKeyDown={onKeyDown}
+                placeholder={questionMode ? questionPlaceholder : placeholder}
+              ></textarea>
+            </div>
+            <div>{loading && "Communing..."}</div>
           </div>
-          <div id="PromptContainer">
-            <textarea
-              rows={8}
-              disabled={loading}
-              ref={textareaRef}
-              onKeyDown={onKeyDown}
-              placeholder="What would you like to do to the sketch?"
-            ></textarea>
-          </div>
-          <div>{loading && "Communing..."}</div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
